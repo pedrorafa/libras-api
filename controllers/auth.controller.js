1f344883071977b066d148b71a7dd0973bedbc98
pedrorafa <pedrorafag00@gmail.com>
2019-11-20 11:39:28 -0300

    auth update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,16 +7,22 @@ let userModel = require('../model/users').model
 
 router.post('/login', (req, res, next) => {
     userModel.findOne({ username: req.body.username }, (err, user) => {
+        if (user === null) {
+            res.status(500).send('Login inválido!');
+            next()
+        }
         let hash = req.body.hash
         if (hash == user.hash) {
             //auth ok
-            const id = user.id; 
+            const id = user.id;
             var token = jwt.sign({ id }, process.env.API_SECRET, {
                 expiresIn: 300 // expires in 5min
             });
             res.status(200).send({ auth: true, token: token });
+            next()
         }
         res.status(500).send('Login inválido!');
+        next()
     })
 })
 router.get('/logout', (req, res) => {
